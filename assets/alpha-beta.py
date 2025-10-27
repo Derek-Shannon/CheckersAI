bd13e18eca4cb7845979c896d905b073c1d176f6
Big guy <jacob.miller.10@und.edu>
2025-10-27 15:26:23 -0500

    Update alpha-beta.py
    added a minmax algorithm (no pruning yet)

--- assets/alpha-beta.py
+++ assets/alpha-beta.py
@@ -1,3 +1,53 @@
-print("search algorithm lol")
+import math
 
-#imagine a really cool and complicated but working search algorithm that's the coolest hting you have ever seen
+
+''' NOTES!!!!!
+uses cords for pieces
+
+already has get_valid_moves, forced to jump
+scores go to 12
+
+
+Ok so idea time
+#start at 0 for score (-24 to +24)
+#check each pieces valid moves, then checks each valid move for valid move. do this until you reach depth. each piece has possible 4 moves (if kinged)
+#picks move that has a biggest number (piece is worth 1 king is worth 2. Nothing is worth 0) (negative values if you lose a piece)
+#pruning using alpha-beta (so if left tree > right tree, don't need to check right anymore)
+
+
+'''
+
+
+def move():
+    best_value = -math.inf
+    best_node = None
+    for node in moves:
+        node.value = minimax(node,3,True)
+        if (node.value > best_value):
+            best_value = node.value
+            best_node = node
+    return best_node
+
+def get_children(node):
+    return node.children
+
+
+
+
+#no pruning yet
+def minimax(node,depth, maximizing_player):
+    if depth == 0:
+        return node.value
+    
+    if maximizing_player: #black
+        best_value = -math.inf
+        for child in get_children(node):
+            value = minimax(child, depth-1, False)
+            best_value = max(best_value,value)
+        return best_value
+    else: #red
+        best_value = math.inf
+        for child in get_children(node):
+            value = minimax(child, depth-1, True)
+            best_value = min(best_value,value)
+        return best_value
